edbc6e1a0027c6c8cebcd6f47e4a3ae55781a3b4
VaustXIII <VaustXIII@gmail.com>
2022-02-19 20:27:35 +0300

    implement flying saucer

---
+++ Asteroids/Assets/Scripts/FlyingSaucerBehaviour.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyingSaucerBehaviour : MonoBehaviour, IShootable {
+    [SerializeField] private float maxSpeed = 10f;
+
+    private Transform target;
+    private Vector2 velocity;
+
+    private void Start() {
+        Initialize(FindObjectOfType<PlayerShipBehaviour>().transform);
+    }
+
+    private void Update() {
+        Move();
+    }
+
+    public void Initialize(Transform target) {
+        this.target = target;
+    }
+
+    public void GetShot() {
+        Destroy(gameObject);
+    }
+
+    private void Move() {
+        if (target == null) {
+            return;
+        }
+
+        velocity = target.position - transform.position;
+        velocity.Normalize();
+        velocity *= maxSpeed;
+
+        var positionDelta = Time.deltaTime * velocity;
+        transform.Translate(positionDelta);
+    }
+
+}
