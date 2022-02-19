9f80b90f6b403d2532660c03ad98aac4ec5ad1bc
VaustXIII <VaustXIII@gmail.com>
2022-02-19 15:33:08 +0300

    add wrapping behaviour

---
+++ Asteroids/Assets/Scripts/WrapAroundBehaviour.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class WrapAroundBehaviour : MonoBehaviour {
+    private SpriteRenderer[] spriteRenderers;
+    private Camera mainCamera;
+
+    private bool isWrappingX;
+    private bool isWrappingY;
+
+    private void Start() {
+        mainCamera = Camera.main;
+        spriteRenderers = GetComponentsInChildren<SpriteRenderer>();
+        Assert.AreNotEqual(spriteRenderers.Length, 0,
+            $"{gameObject.name}.{this.GetType()}: expected to have some sprite renderers on a wrap-around object");
+    }
+
+    private void Update() {
+        if (IsOffscreen()) {
+            WrapAround();
+        }
+        else {
+            isWrappingX = false;
+            isWrappingY = false;
+            return;
+        }
+
+    }
+
+    private bool IsOffscreen() {
+        foreach (var sprite in spriteRenderers) {
+            if (sprite.isVisible) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private void WrapAround() {
+        if (isWrappingX && isWrappingY) return;
+        var newPosition = transform.position;
+
+        var viewPortPosition = mainCamera.WorldToViewportPoint(transform.position);
+        if (!isWrappingX && (viewPortPosition.x > 1.0f || viewPortPosition.x < 0f)) {
+            newPosition.x = -newPosition.x;
+            isWrappingX = true;
+        }
+        if (!isWrappingY && (viewPortPosition.y > 1.0f || viewPortPosition.y < 0f)) {
+            newPosition.y = -newPosition.y;
+            isWrappingY = true;
+        }
+
+        transform.position = newPosition;
+    }
+}
