b603b9ab64dab276d2d241cd8834de495cfeedb2
VaustXIII <VaustXIII@gmail.com>
2022-02-19 11:51:35 +0300

    test

---
+++ Asteroids/Assets/Scripts/Test.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    private void Start()
+    {
+        Debug.Log("Test 1 2 3");
+    }
+
+    private void Update()
+    {
+        
+    }
+}
