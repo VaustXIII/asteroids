7b257319abdd144a464038399bbdec50da86b611
VaustXIII <VaustXIII@gmail.com>
2022-02-23 22:26:09 +0300

    fix nullref

--- Asteroids/Assets/Scripts/Model/FlyingSaucerMovementData.cs
+++ Asteroids/Assets/Scripts/Model/FlyingSaucerMovementData.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/FlyingSaucerMovementData")]
 public class FlyingSaucerMovementData : ScriptableObject {
     public float maxSpeed = 10f;
 }
