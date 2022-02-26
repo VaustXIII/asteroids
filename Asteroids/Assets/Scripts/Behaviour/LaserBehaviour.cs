00162cc26c6b0ba1b2020aa7e3f78483c06fdd94
VaustXIII <VaustXIII@gmail.com>
2022-02-26 17:48:39 +0300

    separate laser logic

--- Asteroids/Assets/Scripts/Behaviour/LaserBehaviour.cs
+++ Asteroids/Assets/Scripts/Behaviour/LaserBehaviour.cs
@@ -1,36 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class LaserBehaviour : MonoBehaviour {
     [SerializeField] private LaserData laserData;
 
-    private int currentChargesCount;
-    private float currentChargeCooldown;
-
-    private SpriteRenderer sprite;
-    private BoxCollider2D collider;
-    private bool isActive = false;
+    private LaserLogic logic;
 
     private Transform FirePoint => transform.parent;
 
-    public int CurrentChargesCount => currentChargesCount;
-    public float CurrentChargeCooldown => currentChargeCooldown;
+    public int CurrentChargesCount => logic.CurrentChargesCount;
+    public float CurrentChargeCooldown => logic.CurrentChargeCooldown;
 
     private void Awake() {
-        sprite = this.GetRequiredComponentInChildren<SpriteRenderer>();
-        collider = this.GetRequiredComponent<BoxCollider2D>();
+        var sprite = this.GetRequiredComponentInChildren<SpriteRenderer>();
+        var collider = this.GetRequiredComponent<BoxCollider2D>();
 
         transform.localScale = new Vector3(laserData.beamWidth, laserData.beamLength, 1f);
         transform.position = FirePoint.position + 0.5f * laserData.beamLength * FirePoint.up;
 
-        currentChargesCount = laserData.maxChargesCount;
-        Deactivate();
+        logic = new LaserLogic(laserData, this, FirePoint, sprite, collider);
     }
 
     private void Update() {
-        Cooldown();
-        HitTargets();
+        logic.Tick(Time.deltaTime);
     }
 
     private void OnCollisionEnter2D(Collision2D collision) {
@@ -40,44 +31,6 @@ public class LaserBehaviour : MonoBehaviour {
     }
 
     public void Shoot() {
-        if (currentChargesCount <= 0) { return; }
-        if (isActive) { return; }
-        currentChargesCount--;
-        Activate();
-        this.Invoke(Deactivate, laserData.activeDuration);
-    }
-
-    private void HitTargets() {
-        if (!isActive) { return; }
-
-        var beamCentre = FirePoint.position + 0.5f * laserData.beamLength * FirePoint.up;
-        var beamSize = new Vector3(laserData.beamWidth, laserData.beamLength, 1f);
-        var angle = FirePoint.rotation.z;
-        Physics2D.OverlapBox(beamCentre, beamSize, angle);
-    }
-
-    private void Cooldown() {
-        if (currentChargesCount >= laserData.maxChargesCount) {
-            currentChargeCooldown = laserData.chargeCooldown;
-            return;
-        }
-
-        currentChargeCooldown -= Time.deltaTime;
-        if (currentChargeCooldown <= 0) {
-            currentChargesCount++;
-            currentChargeCooldown += laserData.chargeCooldown;  // += чтобы перенести остаток в следующий кадр
-        }
-    }
-
-    private void Activate() {
-        isActive = true;
-        sprite.enabled = true;
-        collider.enabled = true;
-    }
-
-    private void Deactivate() {
-        isActive = false;
-        sprite.enabled = false;
-        collider.enabled = false;
+        logic.Shoot();
     }
 }
---
+++ Asteroids/Assets/Scripts/Logic/LaserLogic.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+
+public class LaserLogic {
+    private LaserData data;
+    private MonoBehaviour laserBehaviour;
+    private SpriteRenderer sprite;
+    private BoxCollider2D collider;
+    private Transform firePoint;
+
+    private int currentChargesCount;
+    private float currentChargeCooldown;
+    private bool isActive = false;
+
+    public int CurrentChargesCount => currentChargesCount;
+    public float CurrentChargeCooldown => currentChargeCooldown;
+
+    public LaserLogic(LaserData data, MonoBehaviour laserBehaviour, Transform firePoint, SpriteRenderer sprite, BoxCollider2D collider) {
+        this.data = data;
+        this.laserBehaviour = laserBehaviour;
+        this.firePoint = firePoint;
+        this.sprite = sprite;
+        this.collider = collider;
+
+        currentChargesCount = data.maxChargesCount;
+
+        Deactivate();
+    }
+
+    public void Tick(float dt) {
+        Cooldown(dt);
+    }
+
+    public void Shoot() {
+        if (currentChargesCount <= 0) { return; }
+        if (isActive) { return; }
+        currentChargesCount--;
+        Activate();
+        laserBehaviour.Invoke(Deactivate, data.activeDuration);
+    }
+
+    private void Cooldown(float dt) {
+        if (currentChargesCount >= data.maxChargesCount) {
+            currentChargeCooldown = data.chargeCooldown;
+            return;
+        }
+
+        currentChargeCooldown -= dt;
+        if (currentChargeCooldown <= 0) {
+            currentChargesCount++;
+            currentChargeCooldown += data.chargeCooldown;  // += чтобы перенести остаток в следующий кадр
+        }
+    }
+
+    private void Activate() {
+        isActive = true;
+        sprite.enabled = true;
+        collider.enabled = true;
+    }
+
+    private void Deactivate() {
+        isActive = false;
+        sprite.enabled = false;
+        collider.enabled = false;
+    }
+}
